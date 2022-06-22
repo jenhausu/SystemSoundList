1bc4dc16257b405929c2aa6f1e3526f91edb37f6
蘇健豪 <jenhausu@osensetech.com>
2022-06-22 17:17:25 +0800

    Initial Commit

---
+++ SystemSoundList/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SystemSoundList
+//
+//  Created by 蘇健豪 on 2022/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SystemSoundList/SystemSoundListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemSoundListApp.swift
+//  SystemSoundList
+//
+//  Created by 蘇健豪 on 2022/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SystemSoundListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
